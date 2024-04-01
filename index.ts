892c4180ab68c4610a71ae0866d227c4f4898586
Engrimtiazahmed <160152394+Engrimtiazahmed@users.noreply.github.com>
2024-04-01 12:51:46 +0500

    edited minor changes

--- index.ts
+++ index.ts
@@ -1,65 +1,70 @@
 #! /usr/bin/env node
 //atm machine
-console.log("Dear Custmer Wel come to Imtiaz Generated ATM");
-import inquirer from "inquirer"
+console.log("\n Dear Custmer Wel come to Imtiaz Generated ATM \n");
 
-let myBalance = 10000; 
+import inquirer from "inquirer";
+
+
+let myBalance = 20000;
 let myPin = 3321;
 
-let pinAnswer = await inquirer.prompt([
-  {
-    name: "pin",
-    message: "please insert your Pin Code",
-    type: "number",
-  },
-]);
 
+let pinAnswer = await inquirer.prompt(
+    {
+        message: "Enter Your Pin Code:",
+        name: "pin",
+        type: "number",
+    }
+);
 if (pinAnswer.pin === myPin) {
-  console.log("correct pin number!!!");
+  console.log("Correct Pin code!!! \n");
   let operationAns = await inquirer.prompt([
     {
       name: "operation",
-      message: "please select option",
+      message: "select on of the option: \n",
       type: "list",
-      choices: ["fast cash", "withdraw", "check balance"],
+      choices: ["fast cash", "with draw", "check Balance"],
     },
   ]);
 
-  if (operationAns.operation === "withdraw") {
-    let amountAns = await inquirer.prompt([
+  if (operationAns.operation === "fast cash") {
+    let fastAmountAns = await inquirer.prompt([
       {
-        name: "amount",
-        message: "enter your amount",
-        type: "number",
-      },
-    ]);
-    if (amountAns.amount > myBalance) {
-      console.log("Insufficient funds. Please try a smaller amount.");
-    } else {
-      myBalance -= amountAns.amount;
-      console.log(`Your remaining balance is ${myBalance}`);
-    }
-  } else if (operationAns.operation === "fast cash") {
-    let fastCashAns = await inquirer.prompt([
-      {
-        name: "fastCash",
-        message: "please select an operation",
+        message: "select your amount: \n",
+        name: "fastAmount",
         type: "list",
         choices: ["1000", "2000", "3000", "5000", "10000"],
       },
     ]);
 
-    let selectedAmount = parseInt(fastCashAns.fastCash);
-    if (selectedAmount > myBalance) {
-      console.log("insuficinet balance. Please try a smaller amount.");
+    myBalance -= fastAmountAns.fastAmount;
+    console.log("Your remaining balance is: \n" + myBalance);
+  }
+
+  if (operationAns.operation === "with draw") {
+    let amountAns = await inquirer.prompt([
+      {
+        message: "Enter your amount: \n",
+        name: "amount",
+        type: "number",
+      },
+    ]);
+    if (amountAns.amount > myBalance) {
+      console.log("\n your current balance is Insufficient \n");
     } else {
-      myBalance -= selectedAmount;
-      console.log(`Your remaining balance is ${myBalance}`);
+      myBalance -= amountAns.amount;
+      console.log(`\n Your remaining balance is: ${myBalance} \n` );
     }
-  } else if (operationAns.operation === "check balance") {
-    console.log(`your account balance is ${myBalance}`);
+  } else if (operationAns.operation === "check Balance") {
+    console.log(`\n Your Current Balance is : ${myBalance} \n`);
   }
 } else {
-  console.log("Incorrect Pin Code");
+  console.log("Incorrect Pin code !!!! \n");
 }
-console.log("Thank you from Imtiaz Genearted ATM");
+
+
+console.log("Thank you from Imtiaz Genearted ATM \n");
+
+
+
+
